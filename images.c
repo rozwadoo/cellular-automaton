cee3eec853cf161aaf83538345ba360cc9bebc4c
kuniwyl <wylukkozak1@gmail.com>
2021-03-10 20:48:59 +0100

    po uruchonieniu program stworzy folder Stworzone_pliki a w nim grafiki z rozszerzeniem .pbm

--- images.c
+++ images.c
@@ -12,6 +12,7 @@
 //Przygotowanie do tworzenia plików
 int numer=1;
 int i;
+char *przed = "Stworzone_pliki/";
 char *kon = ".pbm";
 char nazwa[100];
 char nr[10];
@@ -20,13 +21,29 @@ void to_pbm(matrix_t * m, int g, char *f)
 {
 	//Tworzenie nazw plików
 	strcpy(nazwa, "");
+	strcpy(nazwa, przed);
 	strcat(nazwa, f);
 	sprintf(nr, "%i", numer++);
 	strcat(nazwa, nr);
 	strcat(nazwa, kon);
-	printf("%s\n", nazwa);
 	
 	//Tworzenie plików
-		
+	FILE *gra = fopen(nazwa, "w");
+	fprintf(gra, "P1\n"); 	
+	fprintf(gra, "#komentarz\n"); 	
+	fprintf(gra, "%d %d\n", m->rn, m->cn); 		
+	int i,j;
+	for(i = 0; i < m->rn; i++)
+	{
+		for(j = 0; j < m->cn; j++)
+		{
+			if(m->e[i * m->cn + j] == 2)
+				fprintf(gra, "%d ", 1);
+			else
+				fprintf(gra, "%d ", 0);
+		}
+		fprintf(gra, "\n");
+	}	
+	fclose(gra);
 }
 
